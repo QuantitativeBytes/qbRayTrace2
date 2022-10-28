c85a0747d0e330f881055829714c77253cd2f16a
QuantitativeBytes <QuantitativeBytes@gmail.com>
2022-10-28 16:18:56 +0100

    Modified qbutils.hpp to include the definition of the hitData_t data structure.

--- qbRayTrace/qbutils.hpp
+++ qbRayTrace/qbutils.hpp
@@ -5,6 +5,22 @@
 
 namespace qbRT
 {
+	// Forward-declare the object base class.
+	class ObjectBase;
+
+	namespace DATA
+	{
+		struct hitData_t
+		{
+			qbVector<double> poi			{3};
+			qbVector<double> normal		{3};
+			qbVector<double> color		{3};
+			qbVector<double> localPOI	{3};
+			qbVector<double> uvCoords	{2};
+			std::shared_ptr<qbRT::ObjectBase> hitObject;
+		};
+	}
+
 	namespace UTILS
 	{
 		void PrintVector(const qbVector<double> &inputVector);
