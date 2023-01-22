58a50d383a16b342c77d2a8ccfe16672fe36fed2
QuantitativeBytes <QuantitativeBytes@gmail.com>
2023-01-22 12:33:52 +0000

    Minor change to the ComputeReflectionColor function to make sure that it works properly with the new compositebase class.

--- qbRayTrace/qbMaterials/materialbase.cpp
+++ qbRayTrace/qbMaterials/materialbase.cpp
@@ -136,18 +136,29 @@ qbVector<double> qbRT::MaterialBase::ComputeReflectionColor(	const std::vector<s
 		m_reflectionRayCount++;
 		
 		// Check if a material has been assigned.
-		if (closestObject -> m_hasMaterial)
+		//if (closestObject -> m_hasMaterial)
+		if (closestHitData.hitObject -> m_hasMaterial)
 		{
 			// Use the material to compute the color.
+			/*
 			matColor = closestObject -> m_pMaterial -> ComputeColor(	objectList, lightList, 
 																																closestObject, closestHitData.poi, 
 																																closestHitData.normal, reflectionRay);
+			*/
+			matColor = closestHitData.hitObject -> m_pMaterial -> ComputeColor(	objectList, lightList, 
+																																					closestHitData.hitObject, closestHitData.poi, 
+																																					closestHitData.normal, reflectionRay);
 		}
 		else
 		{
+			/*
 			matColor = qbRT::MaterialBase::ComputeDiffuseColor(	objectList, lightList, 
 																													closestObject, closestHitData.poi, 
 																													closestHitData.normal, closestObject->m_baseColor);
+			*/
+			matColor = qbRT::MaterialBase::ComputeDiffuseColor(	objectList, lightList, closestHitData.hitObject, 
+																													closestHitData.poi, closestHitData.normal, 
+																													closestObject->m_baseColor);			
 		}
 	}
 	else
