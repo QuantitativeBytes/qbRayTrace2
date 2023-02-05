c8b602788dfb3bc093c29a774d200c762973da6c
QuantitativeBytes <QuantitativeBytes@gmail.com>
2023-02-05 20:00:42 +0000

    Modified objplane.cpp to make better use of the new qbVector3 class.

--- qbRayTrace/qbPrimatives/objplane.cpp
+++ qbRayTrace/qbPrimatives/objplane.cpp
@@ -68,31 +68,36 @@ bool qbRT::ObjPlane::TestIntersection(	const qbRT::Ray &castRay, qbRT::DATA::hit
 	qbRT::Ray bckRay = m_transformMatrix.Apply(castRay, qbRT::BCKTFORM);
 	
 	// Copy the m_lab vector from bckRay and normalize it.
-	qbVector3<double> k = bckRay.m_lab;
+	//qbVector3<double> k = bckRay.m_lab;
 	// ****
 	//k.Normalize();
 	
 	/* Check if there is an intersection, ie. if the castRay is not parallel
 		to the plane. */
-	if (!CloseEnough(k.GetElement(2), 0.0))
+	//if (!CloseEnough(k.GetElement(2), 0.0))
+	if (!CloseEnough(bckRay.m_lab.GetElement(2), 0.0))
 	{
 		// There is an intersection.
-		double t = bckRay.m_point1.GetElement(2) / -k.GetElement(2);
+		//double t = bckRay.m_point1.GetElement(2) / -k.GetElement(2);
+		double t = bckRay.m_point1.GetElement(2) / -bckRay.m_lab.GetElement(2);
 		
 		/* If t is negative, then the intersection point must be behind
 			the camera and we can ignore it. */
 		if (t > 0.0)
 		{
 			// Compute the values for u and v.
-			double u = bckRay.m_point1.GetElement(0) + (k.GetElement(0) * t);
-			double v = bckRay.m_point1.GetElement(1) + (k.GetElement(1) * t);
+			//double u = bckRay.m_point1.GetElement(0) + (k.GetElement(0) * t);
+			//double v = bckRay.m_point1.GetElement(1) + (k.GetElement(1) * t);
+			double u = bckRay.m_point1.GetElement(0) + (bckRay.m_lab.GetElement(0) * t);
+			double v = bckRay.m_point1.GetElement(1) + (bckRay.m_lab.GetElement(1) * t);
 			
 			/* If the magnitude of both u and v is less than or equal to one
 				then we must be in the plane. */
 			if ((abs(u) < 1.0) && (abs(v) < 1.0))
 			{
 				// Compute the point of intersection.
-				qbVector3<double> poi = bckRay.m_point1 + t * k;
+				//qbVector3<double> poi = bckRay.m_point1 + t * k;
+				qbVector3<double> poi = bckRay.m_point1 + t * bckRay.m_lab;
 				
 				// Transform the intersection point back into world coordinates.
 				hitData.poi = m_transformMatrix.Apply(poi, qbRT::FWDTFORM);
@@ -103,7 +108,8 @@ bool qbRT::ObjPlane::TestIntersection(	const qbRT::Ray &castRay, qbRT::DATA::hit
 				//localNormal = m_transformMatrix.Apply(normalVector, qbRT::FWDTFORM) - globalOrigin;
 				//localNormal.Normalize();
 								
-				qbVector3<double> normalVector {std::vector<double> {0.0, 0.0, -1.0}};
+				//qbVector3<double> normalVector {std::vector<double> {0.0, 0.0, -1.0}};
+				qbVector3<double> normalVector {0.0, 0.0, -1.0};
 				hitData.normal = m_transformMatrix.ApplyNorm(normalVector);
 				hitData.normal.Normalize();
 				
