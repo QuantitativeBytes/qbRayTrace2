afc703b98fe2edc56c31169988d47f37740fdcba
QuantitativeBytes <QuantitativeBytes@gmail.com>
2023-02-28 17:30:48 +0000

    Edits to code comments.

--- CApp.cpp
+++ CApp.cpp
@@ -16,7 +16,7 @@
 	www.youtube.com/c/QuantitativeBytes
 	
 	GPLv3 LICENSE
-
+	Copyright (c) 2023 Michael Bennett
 	
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
